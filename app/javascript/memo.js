c81aa0991ef34cc166710d4e5a0cdf63e375bac7
o-da-a <miu.oda.1381@gmail.com>
2025-05-28 10:11:05 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,19 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;          
+  // ⑩ レスポンスの中から投稿されたメモの情報を抽出、itemに格納 postsコントローラのcreateアクションのrender json: {post: post}で、post=投稿内容に
+       const html = `
+          <div class="post">
+            <div class="post-date">
+              投稿日時：${item.created_at}
+            </div>
+            <div class="post-content">
+              ${item.content}
+            </div>
+          </div>`;
+       return html;
+};
+
+
 function post() {
   const form = document.getElementById("form"); // ① フォーム要素を取得
   form.addEventListener("submit", (e) => {      // ② フォームが送信されたとき
@@ -8,8 +24,19 @@ function post() {
     XHR.open("POST", "/posts", true);           // ⑥ POSTで「/posts」に非同期で送信準備
     XHR.responseType = "json";                  // ⑦ サーバーからのレスポンスの型は JSON にしてほしいと指定
     XHR.send(formData);                         // ⑧ フォームのデータを送信
+  
+    XHR.onload = () => {                        // ⑨ リクエストの送信が成功したら呼び出される、成功後の処理を定義
+      if (XHR.status != 200) {
+         alert(`Error ${XHR.status}: ${XHR.statusText}`); //statusとTextは規定されている内容が勝手に出てくる
+         return null;                           //エラーが出たらJavaScriptの処理から抜け出す
+      }
+      const list = document.getElementById("list");
+       const formText = document.getElementById("content");
+        list.insertAdjacentHTML("afterend", buildHTML(XHR));
+        formText.value = "";
+          };
   });
-}
+};
 
 
 window.addEventListener('turbo:load', post);
